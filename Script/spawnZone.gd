66393898c0408bba5de9f4ebeae6c7b5a0cd2f27
Locke-Lamora0 <contealexandre3@proton.me>
2024-07-13 19:48:08 +0200

    add a menu and create a basic spawn system for different species

--- Script/spawnZone.gd
+++ Script/spawnZone.gd
@@ -1,24 +1,24 @@
 extends Node2D
 
-var npc1_scene = preload("res://assets/animal_test.tscn")
+
+var npc1_scene = preload("res://assets/animal_test.tscn") # on affecte dans cette variable le type de pnj 
 var npc2_scene = preload("res://assets/animal_test_2.tscn")
 
+
 func _ready():
-	for i in 5:
-		var positionsXAnimal1 = randf_range(100, 500)
-		var positionsYAnimal1 = randf_range(100, 500)
-		var positionsXAnimal2 = randf_range(600, 900)
-		var positionsYAnimal2 = randf_range(600, 900)
-		spawn_npc1(Vector2(positionsXAnimal1, positionsYAnimal1))
-		spawn_npc2(Vector2(positionsXAnimal2, positionsYAnimal2))
+# Spawns 5 NPCs of type 1 and defines the range for X and Y coordinates
 
 
-func spawn_npc1(spawn_position: Vector2):
-	var npc_instance = npc1_scene.instantiate()
-	add_child(npc_instance)
-	npc_instance.position = spawn_position
+	spawn_npcs(npc1_scene, 5, Vector2(100, 500), Vector2(100, 500))
+	# Fait apparaître 10 PNJ de type 2
+	spawn_npcs(npc2_scene, 10, Vector2(600, 1000), Vector2(600, 1000))
 
-func spawn_npc2(spawn_position: Vector2):
-	var npc_instance = npc2_scene.instantiate()
-	add_child(npc_instance)
-	npc_instance.position = spawn_position
+# Generic function to prepare and spawn NPCs
+func spawn_npcs(npc_scene, number_of_individuals, x_range: Vector2, y_range: Vector2):
+	for i in range(number_of_individuals):
+		# Generates random X and Y coordinates within the specified ranges
+		var position = Vector2(randf_range(x_range.x, x_range.y), randf_range(y_range.x, y_range.y))
+		# Prepares and spawns the NPC at the generated position
+		var npc_instance = npc_scene.instantiate() # Instantiates the NPC from the preloaded scene
+		add_child(npc_instance) # Adds the NPC to the scene tree
+		npc_instance.position = position # Sets the NPC's position
