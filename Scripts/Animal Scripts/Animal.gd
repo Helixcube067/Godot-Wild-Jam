549b97257a547194917c8e8badf019f7b2432777
Helixcube067 <helixcubethegamer@gmail.com>
2024-07-13 15:00:16 -0400

    Animal movement
    - Added in randomized animal movement

---
+++ Scripts/Animal Scripts/Animal.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+class_name animal
+
+const SPEED = 100.0
+var destination : Vector2
+var rng = RandomNumberGenerator.new()
+var randomX : int
+var randomY : int
+var isMoving : bool = false
+var pauseTime : int
+#if moving is true then it checks to see if you reached the destination and if so turns is moving to false
+#if moving is true and its not at the destination it keeps moving
+#if moving is switched back to false then we get a new destination
+func _physics_process(delta):
+	if(isMoving):
+		if(global_position == destination):
+			isMoving = false
+		else:
+			global_position = global_position.move_toward(destination, delta * SPEED)
+	else:
+		SetNewDestination(delta)
+	move_and_slide()
+
+#Gets the delta to multiply the speedby and turns isMoving to true
+#gets a random X and Y value within the range and adds it to the value of the current positions x and y
+#moves toward that position
+func SetNewDestination(delta):
+	isMoving = true
+	randomX = rng.randi_range(-100,100)
+	randomY = rng.randi_range(-100,100)
+	#can do trimming here to make sure it doesnt go out of bounds
+	destination.x = position.x + randomX
+	destination.y = position.y + randomY
+	printt("Current global: " + str(global_position))
+	printt("Destination: " + str(destination))
+	global_position = global_position.move_toward(destination, delta * SPEED)
