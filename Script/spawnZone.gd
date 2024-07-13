6374e5e6a0a055351f3975a3f8cf1c6f4880b42b
Locke-Lamora0 <contealexandre3@proton.me>
2024-07-13 11:53:51 +0200

    add a menu and create a basic spawn system for different species

--- Script/spawnZone.gd
+++ Script/spawnZone.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-# Chargez la scène du NPC
 var npc1_scene = preload("res://assets/animal_test.tscn")
 var npc2_scene = preload("res://assets/animal_test_2.tscn")
 func _ready():
@@ -12,10 +11,10 @@ func _ready():
 		var positionsYAnimal2 = randf_range(600, 900)
 		spawn_npc1(Vector2(positionsXAnimal1, positionsYAnimal1))
 		spawn_npc2(Vector2(positionsXAnimal2, positionsYAnimal2))
-	# Faites apparaître trois NPCs à des positions différentes
 
 
-# Fonction pour faire apparaître un NPC à une position donnée
+
+
 func spawn_npc1(spawn_position: Vector2):
 	var npc_instance = npc1_scene.instantiate()
 	add_child(npc_instance)
