2c4ee768dcf4284736d67bdabf2342e50e83980a
Helixcube067 <helixcubethegamer@gmail.com>
2024-07-13 20:07:40 -0400

    Prey/Predator changes
    - Added movement and logic for the prey and predator classes
    - Made both predator and prey into prefabs
    - Things to get done: Make a plant class for the prey to eat
    - Potential issues: The method of checking for whats to be eaten is a lil narrow so expanding on this could be difficult

--- Scripts/Animal Scripts/AnimalBase.gd
+++ Scripts/Animal Scripts/AnimalBase.gd
@@ -2,25 +2,22 @@ extends CharacterBody2D
 class_name animalBase
 
 enum type {Prey, Predator}
-const SPEED = 100.0
+var animalType
+@export var hungry : bool = false
+const SPEED = 75.0
 var destination : Vector2
 var rng = RandomNumberGenerator.new()
 var randomX : int
 var randomY : int
 var isMoving : bool = false
 var pauseTime : int
+
 #if moving is true then it checks to see if you reached the destination and if so turns is moving to false
-#if moving is true and its not at the destination it keeps moving
+#if moving is true and its not atthe destination it keeps moving
 #if moving is switched back to false then we get a new destination
-func _physics_process(delta):
-	if(isMoving):
-		if(global_position == destination):
-			isMoving = false
-		else:
-			global_position = global_position.move_toward(destination, delta * SPEED)
-	else:
-		SetNewDestination(delta)
-	move_and_slide()
+#func _physics_process(delta):
+	#MovementCheck(delta)
+	#move_and_collide(destination)
 
 #Gets the delta to multiply the speedby and turns isMoving to true
 #gets a random X and Y value within the range and adds it to the value of the current positions x and y
@@ -32,6 +29,15 @@ func SetNewDestination(delta):
 	#can do trimming here to make sure it doesnt go out of bounds
 	destination.x = position.x + randomX
 	destination.y = position.y + randomY
-	printt("Current global: " + str(global_position))
-	printt("Destination: " + str(destination))
+	#printt("Current global: " + str(global_position))
+	#printt("Destination: " + str(destination))
 	global_position = global_position.move_toward(destination, delta * SPEED)
+
+func MovementCheck(delta):
+	if(isMoving):
+		if(global_position == destination):
+			isMoving = false
+		else:
+			global_position = global_position.move_toward(destination, delta * SPEED)
+	else:
+		SetNewDestination(delta)
--- Scripts/Animal Scripts/Predator.gd
+++ Scripts/Animal Scripts/Predator.gd
@@ -1,1 +1,26 @@
 extends animalBase
+@onready var detectionRange : Area2D = %"Detection Range"
+var actionQueue : Array
+var surroundingsInRange : Array
+
+func _ready():
+	animalType = type.Predator
+	
+func _physics_process(delta):
+	if(hungry):
+		SeekFood(delta)
+		var collision : bool = move_and_slide()
+		if(collision):
+			if(get_last_slide_collision().get_collider().animalType == type.Prey):
+				get_last_slide_collision().get_collider().free()
+				hungry = false
+	else:
+		MovementCheck(delta)
+		move_and_slide()
+	
+func SeekFood(delta):
+	surroundingsInRange = detectionRange.get_overlapping_bodies()
+	for i in surroundingsInRange.size():
+		if(surroundingsInRange[i].animalType == type.Prey):
+			destination = surroundingsInRange[i].global_position
+	global_position = global_position.move_toward(destination, delta * SPEED)
